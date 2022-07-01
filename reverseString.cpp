4fa51efd055e2f81a1e9c21d73207213da4aa578
Rafał Ściebura <rafal.sciebura7@gmail.com>
2022-07-01 02:19:09 +0200

    Add new files

---
+++ reverseString.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+
+int main()
+{
+    int i;
+    std::string word;
+    std::cout << "Enter the word to reverse: ";
+    
+    std::getline(std::cin, word);
+
+    int wordLength = word.length();
+
+    for (i = wordLength - 1; i >= 0; i--) {
+        std::cout << word[i];
+    }
+    
+    return 0;
+}
